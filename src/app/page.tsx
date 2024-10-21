01649f837295526e09d715ab3b785cf2676760e4
adilrana03 <adilrana2014@gmail.com>
2024-10-22 00:28:43 +0530

    added line ans coming soon btn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
 
 
 	return (
-		<div className='lg:p-8 md:p-6 p-4 bg-blue-100 3xl:min-h-screen min-h-screen items-center flex'>
+		<div className='lg:p-8 md:p-6 p-4 bg-[#E5E8FF] 3xl:min-h-screen min-h-screen items-center flex'>
 			<EcosystemMap />
 		</div>
 	);
---
+++ src/components/LiveIndicator.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+const LiveIndicator = () => {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			width='20'
+			height='20'
+			viewBox='0 0 20 20'
+			fill='none'
+			className='live-indicator'>
+			<circle
+				cx='10'
+				cy='10'
+				r='7.5'
+				fill='white'
+				fillOpacity='0.83'
+				className='outer-circle'
+			/>
+			<circle
+				cx='10'
+				cy='10'
+				r='5'
+				fill='#D91538'
+				className='inner-circle'
+			/>
+			<style jsx>{`
+				@keyframes pulse {
+					0% {
+						transform: scale(1);
+						opacity: 1;
+					}
+					50% {
+						transform: scale(1.1);
+						opacity: 0.7;
+					}
+					100% {
+						transform: scale(1);
+						opacity: 1;
+					}
+				}
+				.live-indicator {
+					animation: pulse 2s infinite;
+				}
+				.outer-circle {
+					animation: pulse 1s infinite;
+				}
+				.inner-circle {
+					animation: pulse 2s infinite 0.2s;
+				}
+			`}</style>
+		</svg>
+	);
+};
+
+export default LiveIndicator;
